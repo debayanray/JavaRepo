0487ee11642a88dfde0b1f668e19075d1d933a17
subhadip Das <subhadip.das@perceptivesoftware.com>
2014-11-03 18:51:49 +0530

    Source modified by subahdip

--- SmartVehicleAnalytics/src/smart/vehicle/analytics/main/Main.java
+++ SmartVehicleAnalytics/src/smart/vehicle/analytics/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Our smart vehicle analytics");
+		System.out.println("Our smart vehicle analytics from subhadip");
 	}
 
 }
